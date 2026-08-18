857331c57ead55b1f8cf163dfb3517e0a694dfd6
favourkendi-dev <favour.kendi@student.moringaschool.com>
2026-08-18 15:34:12 +0300

    Add placeholder Navbar, Home, AppRoutes and install react-router-dom

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#141414] p-4 text-white">
+      <h1 className="text-amber-500 font-bold">Cinéma</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-white">Home </h2>
+    </div>
+  );
+};
+
+export default Home;
