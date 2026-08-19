52fc26641137334e5a0ac816207f0dfd9eaaffc3
Fideles njoki <fideles.njoki@student.moringaschool.com>
2026-08-20 00:35:35 +0300

    created the Authentication page and resolved merge conflicts

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cinema: '#f6b042'
+      }
+    }
+  },
+  plugins: [],
+}
